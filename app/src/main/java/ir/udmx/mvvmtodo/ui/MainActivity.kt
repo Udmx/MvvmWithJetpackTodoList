9c1d7b6fffc8d44778e215e58080054ea51487bf
Pouria Shafiee <shafieeofficial@gmail.com>
2020-11-28 11:01:40 +0330

    Add Model and set NavigationView

---
+++ app/src/main/java/ir/udmx/mvvmtodo/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ir.udmx.mvvmtodo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.udmx.mvvmtodo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/ir/udmx/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -0,0 +1,8 @@
+package ir.udmx.mvvmtodo.ui.tasks
+
+import androidx.fragment.app.Fragment
+import ir.udmx.mvvmtodo.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+
+}
