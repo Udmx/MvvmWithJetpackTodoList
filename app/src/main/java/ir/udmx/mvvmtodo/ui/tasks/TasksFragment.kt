690668da827967deadef8ed7b4a6a332c8b8587c
Pouria Shafiee <shafieeofficial@gmail.com>
2020-12-02 17:29:18 +0330

    add and set recyclerview and viewmodel with coroutines

--- app/src/main/java/ir/udmx/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/ir/udmx/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -1,11 +1,34 @@
 package ir.udmx.mvvmtodo.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import ir.udmx.mvvmtodo.R
+import ir.udmx.mvvmtodo.databinding.FragmentTasksBinding
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
-    private val viewModel : TasksViewModel by viewModels()
+    private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val tasksAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = tasksAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            tasksAdapter.submitList(it)
+        }
+    }
 }
--- app/src/main/java/ir/udmx/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/ir/udmx/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,12 @@ package ir.udmx.mvvmtodo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import ir.udmx.mvvmtodo.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
